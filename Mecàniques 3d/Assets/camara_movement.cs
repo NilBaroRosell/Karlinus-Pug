af44de2026ad7597881083dec279078c218c7acf
nilbaro <nil.baro@enti.cat>
2018-10-10 10:02:46 +0200

    Moviment personatge i càmara

---
+++ Mecàniques 3d/Assets/camara_movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camara_movement : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject reference;
+    private Vector3 distance;
+
+	// Use this for initialization
+	void Start () {
+        distance = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        distance = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * 2, Vector3.up) * distance;
+        transform.position = player.transform.position + distance;
+        transform.LookAt(player.transform.transform.position);
+
+        Vector3 copyRotation = new Vector3(0, transform.eulerAngles.y, 0);
+        reference.transform.eulerAngles = copyRotation;
+	}
+}
---
+++ Mecàniques 3d/Assets/movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public int speed;
+    public GameObject reference;
+
+    bool onFloor = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if(rb.velocity.magnitude > speed) rb.velocity = rb.velocity.normalized * speed;
+
+        rb.AddForce(moveVertical * reference.transform.forward * speed);
+        rb.AddForce(moveHorizontal * reference.transform.right * speed);
+
+        transform.localRotation = reference.transform.localRotation;
+
+        if (Input.GetKey(KeyCode.Space) && onFloor)
+        {
+            rb.AddForce(new Vector3(0, 430, 0));
+            onFloor = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "floor") onFloor = true;
+    }
+}
