53396e389affed4ab33be968dbe391ebc71cda8c
nilbaro <nil.baro@enti.cat>
2018-10-14 18:06:37 +0200

    Dash y movimiento
    Dash creado y movimiento retocado

--- Mecàniques 3d/Assets/movement.cs
+++ Mecàniques 3d/Assets/movement.cs
@@ -10,8 +10,11 @@ public class movement : MonoBehaviour
     static Animator anim;
 
     public int speed;
+    public int dashSpeed;
 
     public bool onFloor = false;
+
+    public string direction;
 
     // Use this for initialization
     void Start()
@@ -32,10 +35,60 @@ public class movement : MonoBehaviour
 
         transform.rotation = reference.transform.rotation;
 
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            switch (direction)
+            {
+                case "F":
+                {
+                    transform.Rotate(0, 0, 0);
+                    break;
+                }
+                case "B":
+                {
+                    transform.Rotate(0, 180, 0);
+                    break;
+                }
+                case"L":
+                {
+                    transform.Rotate(0, -90, 0);
+                    break;
+                }
+                case "R":
+                {
+                    transform.Rotate(0, 90, 0);
+                    break;
+                }
+                case "FL":
+                {
+                    transform.Rotate(0, -45, 0);
+                    break;
+                }
+                case "FR":
+                {
+                    transform.Rotate(0, 45, 0);
+                    break;
+                }
+                case "BL":
+                {
+                    transform.Rotate(0, -135, 0);
+                    break;
+                }
+                case "BR":
+                {
+                    transform.Rotate(0, 135, 0);
+                    break;
+                }
+                default: break;
+            }
+            rb.AddForce(moveVertical * reference.transform.forward * dashSpeed);
+            rb.AddForce(moveHorizontal * reference.transform.right * dashSpeed);
+        }
+
         if (Input.GetKeyDown(KeyCode.Space) && onFloor)
         {
             anim.SetTrigger("Is_Jumping");
-            rb.AddForce(new Vector3(0, 300, 0));
+            rb.AddForce(new Vector3(0, 275, 0));
             onFloor = false;
         }
 
@@ -44,50 +97,103 @@ public class movement : MonoBehaviour
             transform.Rotate(0, 45, 0);
             anim.SetBool("Is_Walking", true);
             anim.SetBool("Is_Idle", false);
+            direction = "FR";
         }
         else if (moveVertical > 0 && moveHorizontal < 0)
         {
             transform.Rotate(0, -45, 0);
             anim.SetBool("Is_Walking", true);
             anim.SetBool("Is_Idle", false);
+            direction = "FL";
         }
         else if (moveVertical < 0 && moveHorizontal > 0)
         {
             transform.Rotate(0, 135, 0);
             anim.SetBool("Is_Walking", true);
             anim.SetBool("Is_Idle", false);
+            direction = "BR";
         }
         else if (moveVertical < 0 && moveHorizontal < 0)
         {
             transform.Rotate(0, -135, 0);
             anim.SetBool("Is_Walking", true);
             anim.SetBool("Is_Idle", false);
+            direction = "BL";
         }
         else if (moveVertical > 0)
         {
             anim.SetBool("Is_Walking", true);
             anim.SetBool("Is_Idle", false);
+            direction = "F";
         }
         else if (moveVertical < 0)
         {
             transform.Rotate(0, 180, 0);
             anim.SetBool("Is_Walking", true);
             anim.SetBool("Is_Idle", false);
+            direction = "B";
         }
         else if (moveHorizontal > 0)
         {
             transform.Rotate(0, 90, 0);
             anim.SetBool("Is_Walking", true);
             anim.SetBool("Is_Idle", false);
+            direction = "R";
         }
         else if (moveHorizontal < 0)
         {
             transform.Rotate(0, -90, 0);
             anim.SetBool("Is_Walking", true);
             anim.SetBool("Is_Idle", false);
+            direction = "L";
         }
         if (moveVertical == 0 && moveHorizontal == 0)
         {
+            switch (direction)
+            {
+                case "F":
+                    {
+                        transform.Rotate(0, 0, 0);
+                        break;
+                    }
+                case "B":
+                    {
+                        transform.Rotate(0, 180, 0);
+                        break;
+                    }
+                case "L":
+                    {
+                        transform.Rotate(0, -90, 0);
+                        break;
+                    }
+                case "R":
+                    {
+                        transform.Rotate(0, 90, 0);
+                        break;
+                    }
+                case "FL":
+                    {
+                        transform.Rotate(0, -45, 0);
+                        break;
+                    }
+                case "FR":
+                    {
+                        transform.Rotate(0, 45, 0);
+                        break;
+                    }
+                case "BL":
+                    {
+                        transform.Rotate(0, -135, 0);
+                        break;
+                    }
+                case "BR":
+                    {
+                        transform.Rotate(0, 135, 0);
+                        break;
+                    }
+                default:
+                    break;
+            }
             anim.SetBool("Is_Walking", false);
             anim.SetBool("Is_Idle", true);
         }
