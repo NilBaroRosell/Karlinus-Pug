7df0de4dd890b63dbda791a2a8426470bcbcb3ef
nilbaro <nil.baro@enti.cat>
2018-11-27 14:23:11 +0100

    cambios en la camara

---
+++ Mecàniques 3d/Assets/referenceMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class referenceMovement : MonoBehaviour {
+
+    public GameObject player;
+    private movement getJumping;
+    public bool onFloor = true;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        getJumping = player.GetComponent<movement>();
+        onFloor = getJumping.onFloor;
+
+        if (onFloor) transform.position = player.transform.position;
+        else transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+	}
+}
