e9015ab1c17a51d21dacb46587f0764862c12cd2
nilbaro <nil.baro@gmail.com>
2019-04-06 13:24:06 +0200

    Pequeños avances creditos

---
+++ Mecàniques 3d/Assets/CreditsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ExecuteAfterTime(16));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        
+    }
+}
